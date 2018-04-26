52d3d02ee7f0be7bf674212aceb8eb846c6eab01
Asaf Davidov <adavidov@conncoll.edu>
2018-04-26 21:34:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // write your code below!
-function happyHolidays{
+function happyHolidays(){
   var hapHolidayString = "Happy holidays!";
   return hapHolidayString;
 }
+
 function happyHolidaysTo(name){
   var hapHolidayStringTo = 'Happy holidays, ${name}!'
   return hapHolidayStringTo;
