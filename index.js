a515923cdbb5b1189e889f048a61a7e627287a9c
Asaf Davidov <adavidov@conncoll.edu>
2018-04-26 21:39:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  var name2 = name;
-  var hapHolidayStringTo = 'Happy holidays, ${name2}!';
+  var hapHolidayStringTo = 'Happy holidays, ' + name + '!';
   return hapHolidayStringTo;
 }
